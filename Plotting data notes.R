e84e446a7a4bd768f14277cd7d7fe741a0f1665b
WillBolland <williambolland98@gmail.com>
2021-05-28 17:28:42 +0100

    Updated plotting notes

--- Plotting data notes.R
+++ Plotting data notes.R
@@ -1,24 +1,24 @@
-pch: the plotting symbol
-lty: the line type
-lwd: line width
-col: plotting colour
-xlab: x axis label
-ylab: y axis label
-las: orientation of the axis on the plot
-bg: background colours
-mar: margin size
-oma: outer margin size
-mfrow: number of plots per row
-mfcol: number of plots per column
+#pch: the plotting symbol
+#lty: the line type
+#col: plotting colour
+#lwd: line width
+#ylab: y axis label
+#xlab: x axis label
+#las: orientation of the axis on the plot
+#bg: background colours
+#mar: margin size
+#oma: outer margin size
+#mfrow: number of plots per row
+#mfcol: number of plots per column
 
-**Plotting functions**
-plot: make a scatterplot
-lines: add lines
-points: add points
-text: add text
-title: add title
-mtext: add text to the margin
-axis: adding axis label/ticks
+#**Plotting functions**
+#plot: make a scatterplot
+#lines: add lines
+#points: add points
+#text: add text
+#title: add title
+#mtext: add text to the margin
+#axis: adding axis label/ticks
 legend("topright", pch = 1 , col = c("red", "blue"), legend = c("May", "Other months")
 abline(model, lwd = 2)
 
@@ -29,5 +29,5 @@ with(airquality, {
   mtext("London climate", outer = TRUE)
 })
 
-lm: makes a linear model between variables
-abline(linearmodelname): adds the line for the linear model
+#lm: makes a linear model between variables
+#abline(linearmodelname): adds the line for the linear model
